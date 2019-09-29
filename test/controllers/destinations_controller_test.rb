2cdf5c0042d9805819516afa4b6be4975a421e73
DankoMaksym <dankomaksym@gmail.com>
2019-09-29 16:28:58 +0300

    Ready to deploy

---
+++ test/controllers/destinations_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class DestinationsControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get destinations_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get destinations_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get destinations_update_url
+    assert_response :success
+  end
+
+end
